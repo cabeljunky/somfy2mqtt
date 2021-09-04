5a44c08a41fe517b543bac21834399ae66ef6f08
Edward Heuveling <ea.heuveling@red-software.nl>
2021-09-04 14:27:23 +0200

    Update the PiGPIO interface and getting the evn arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 : "${PIGPIOD_ARGS:-}"
 : "${PIGPIO_HOST:-}"
 : "${PIGPIO_PORT:-}"
-: "${CONFIG:="-config /opt/somfy/config/operateShutters.conf"}"
+: "${CONFIG:="/opt/somfy/config/operateShutters.conf"}"
 
 #while true; do
 #  sleep 100
 #done
 
-/usr/bin/python3 /opt/somfy/operateShutters.py -auto -mqtt "${CONFIG}"
+/usr/bin/python3 /opt/somfy/operateShutters.py -auto -mqtt -config ${CONFIG}
