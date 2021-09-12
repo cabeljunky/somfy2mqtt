486e2df15d53ae0fb6e0c60436a1c5659e986cf8
Edward Heuveling <ea.heuveling@red-software.nl>
2021-09-12 21:39:26 +0200

    Refactored logging to console

--- mylog.py
+++ mylog.py
@@ -76,8 +76,8 @@ class MyLog(object):
     # ---------------------MyLog::LogConsole------------------------------------
     def LogConsole(self, Message):
         print(Message);
-        if self.log is not None:
-            self.console.error(Message)
+        if self.console is not None:
+            self.console(Message)
 
     # ---------------------MyLog::LogError------------------------------------
     def LogError(self, Message):
